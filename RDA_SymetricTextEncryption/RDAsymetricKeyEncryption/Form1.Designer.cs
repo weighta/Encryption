6437694f7a1e6bfbdb807b3e55ec74176bd82777
Alex Weight <weighta@hotmail.com>
2018-05-26 18:14:26 -0700

    RDA_SymmetricTextEncryption
    RoboCrypto's big sister... The revised text encryption algorithms, which that can be enough to secure any text against the public.

---
+++ RDA_SymetricTextEncryption/RDAsymetricKeyEncryption/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace RDAsymetricKeyEncryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(2, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(274, 115);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Silver;
+            this.textBox1.Location = new System.Drawing.Point(282, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "key...";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.NameText_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.NameText_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(282, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(287, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(363, 91);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(58, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Values";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "len: ";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(363, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "R Function";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(516, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "bits: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(282, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Hash:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(2, 121);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(274, 115);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "RHash:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(615, 234);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "RDA Symetric Key Encryption";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ RDA_SymetricTextEncryption/RDAsymetricKeyEncryption/Form1.cs
@@ -0,0 +1,511 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+/*
+ * RDA ENCRYPTION; THE 6 STEPS - REVISED
+ * 1. Reverse blocks of 54 bits (6 chars)
+ * 2. Apply the R sum function for adding round hash/key //STORE OUTPUT AS 16 OCTETS
+ * 
+ * 6-8 ROUNDS {
+ * 3. Shift Rows of 128 bit text (16 chars)
+ * 4. Add Round Key //XOR 16 Octets with hash
+ * 5. Reverse Matrix (128 bit text)
+ * }
+ * 6. Initialization //Get initialization vector from revised vector algorithm.
+*/
+
+
+
+
+
+
+
+namespace RDAsymetricKeyEncryption
+{
+    public partial class Form1 : Form
+    {
+        Random ran = new Random();
+        private char[] ru = "Ruthie".ToCharArray();
+        private char[] ru25 = "basketballru25".ToCharArray();
+        private char[] key;
+
+        private string splitinfRHash;
+        private string[] splitinfRHash2;
+        private byte[] infRHash;
+
+        private string sRHash;
+        private string splitRHash;
+        private string[] splitRHash2;
+        private byte[] RHash;
+
+        private string sKHash;
+        private string splitKHash;
+        private string[] splitKHash2;
+        private byte[] KHash;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NameText_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "key...")
+            {
+                textBox1.Clear();
+            }
+        }
+
+        private void NameText_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.ForeColor = Color.Silver;
+                textBox1.Text = "key...";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sRHash = "";
+            splitRHash = "";
+            splitinfRHash = "";
+
+            HashingAlgorithm();
+            richTextBox2.Clear();
+            key = label3.Text.ToCharArray();
+
+            string array;
+            char[] arrayreverse;
+
+            string reversedblocks = "";
+            string Rfunc = "";
+            long r25 = 0;
+            long r = 0;
+            long s = 0;
+            long l = key.Length;
+            long n;
+            long mod;
+            int addindex = 0;
+            int f = 0;
+            int stop = 0;
+            foreach (char i in ru) r += i;
+            foreach (char i in key) s += i;
+            foreach (char i in ru25) r25 += i;
+            if (richTextBox1.Text.Length != 0)
+            {
+                //reverse columns
+                while (true)
+                {
+                    array = "";
+                    for (int i = 0; i <= 5; i++)
+                    {
+                        try
+                        {
+                            array += richTextBox1.Text[i + addindex];
+                        }
+                        catch
+                        {
+                            stop = 1;
+                            break;
+                        }
+                    }
+                    arrayreverse = array.ToCharArray();
+                    Array.Reverse(arrayreverse);
+                    for (int i = 0; i < arrayreverse.Length; i++)
+                    {
+                        reversedblocks += arrayreverse[i];
+                    }
+                    if (stop == 1)
+                    {
+                        break;
+                    }
+                    addindex += 6;
+                }
+                //The R sum function step
+                for (int i = 0; i < reversedblocks.Length; i++)
+                {
+                    n = 0;
+                    for (int j = 0; j < l; j++)
+                    {
+                        int x = key[j];
+                        n += ((i + x) ^ (s + l) * (long)Math.Pow((Math.Pow(x, 3) % r), 2));
+                        for (int p = 0; p < ru.Length; p++)
+                        {
+                            n ^= ru[p] * (i ^ (n % x + 25));
+                        }
+                        n ^= x * s;
+                        n ^= x * ru[0];
+                        n ^= x * ru[1];
+                        n ^= x * ru[2];
+                        n ^= x * ru[3];
+                        n ^= x * ru[4];
+                        n ^= x * ru[5];
+                    }
+                    mod = n % (r ^ l) + 25;
+                    Rfunc += Convert.ToChar(reversedblocks[i] + mod);
+
+                    //infRHash
+                    splitinfRHash += (mod % 256).ToString("X2");
+                    if (splitinfRHash.Length < reversedblocks.Length * 3 - 1)
+                    {
+                        splitinfRHash += "|";
+                    }
+
+                    //splitRHash
+                    if (splitRHash.Length < 47)
+                    {
+                        splitRHash += (mod % 256).ToString("X2");
+                        sRHash += (mod % 256).ToString("X2");
+                        if (splitRHash.Length < 47)
+                        {
+                            splitRHash += "|";
+                        }
+                    }
+                }
+                splitinfRHash2 = splitinfRHash.Split('|');
+                infRHash = splitinfRHash2.Select(b => Convert.ToByte(b, 16)).ToArray();
+                while (true)
+                {
+                    if (splitRHash.Length < 47)
+                    {
+                        splitRHash += "00";
+                        sRHash += "00";
+                        if (splitRHash.Length < 47)
+                        {
+                            splitRHash += "|";
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                splitRHash2 = splitRHash.Split('|');
+                RHash = splitRHash2.Select(b => Convert.ToByte(b, 16)).ToArray();
+                label4.Text = "RHash: " + sRHash + " len: " + (sRHash.Length * 8);
+
+                richTextBox1.Text = Rfunc;
+                richTextBox2.Text = splitinfRHash.ToString();
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = "len: " + richTextBox1.Text.Length;
+            if (checkBox1.Checked == true)
+            {
+                textBox2.Clear();
+                foreach (char i in richTextBox1.Text)
+                {
+                    textBox2.AppendText((int)i + " ");
+                }
+            }
+            else
+            {
+                textBox2.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sRHash = "";
+            splitRHash = "";
+            splitinfRHash = "";
+
+            HashingAlgorithm();
+
+            string array;
+            string reversedblocks = "";
+            string Rfunc = "";
+            char[] arrayreverse;
+            key = label3.Text.ToCharArray();
+            long r = 0;
+            long r25 = 0;
+            long l = key.Length;
+            long s = 0;
+            long n;
+            long mod;
+            int addindex = 0;
+            int stop = 0;
+            foreach (char i in ru) r += i;
+            foreach (char i in key) s += i;
+            foreach (char i in ru25) r25 += i;
+            try
+            {
+                //The R sum function step
+                for (int i = 0; i < richTextBox1.Text.Length; i++)
+                {
+                    n = 0;
+                    for (int j = 0; j < l; j++)
+                    {
+                        int x = key[j];
+                        n += ((i + x) ^ (s + l) * (long)Math.Pow((Math.Pow(x, 3) % r), 2));
+                        for (int p = 0; p < ru.Length; p++)
+                        {
+                            n ^= ru[p] * (i ^ (n % x + 25));
+                        }
+                        n ^= x * s;
+                        n ^= x * ru[0];
+                        n ^= x * ru[1];
+                        n ^= x * ru[2];
+                        n ^= x * ru[3];
+                        n ^= x * ru[4];
+                        n ^= x * ru[5];
+                    }
+                    mod = n % (r ^ l) + 25;
+                    Rfunc += Convert.ToChar(richTextBox1.Text[i] - mod);
+
+                    //infRHash
+                    splitinfRHash += (mod % 256).ToString("X2");
+                    if (splitinfRHash.Length < richTextBox1.Text.Length * 3 - 1)
+                    {
+                        splitinfRHash += "|";
+                    }
+
+                    //splitRHash
+                    if (splitRHash.Length < 47)
+                    {
+                        splitRHash += (mod % 256).ToString("X2");
+                        sRHash += (mod % 256).ToString("X2");
+                        if (splitRHash.Length < 47)
+                        {
+                            splitRHash += "|";
+                        }
+                    }
+                }
+                splitinfRHash2 = splitinfRHash.Split('|');
+                infRHash = splitinfRHash2.Select(b => Convert.ToByte(b, 16)).ToArray();
+                while (true)
+                {
+                    if (splitRHash.Length < 47)
+                    {
+                        splitRHash += "00";
+                        sRHash += "00";
+                        if (splitRHash.Length < 47)
+                        {
+                            splitRHash += "|";
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                splitRHash2 = splitRHash.Split('|');
+                RHash = splitRHash2.Select(b => Convert.ToByte(b, 16)).ToArray();
+                label4.Text = "RHash: " + sRHash + " len: " + (sRHash.Length * 8);
+                //reverse columns
+                while (true)
+                {
+                    array = "";
+                    for (int i = 0; i <= 5; i++)
+                    {
+                        try
+                        {
+                            array += Rfunc[i + addindex];
+                        }
+                        catch
+                        {
+                            stop = 1;
+                            break;
+                        }
+                    }
+                    arrayreverse = array.ToCharArray();
+                    Array.Reverse(arrayreverse);
+                    for (int i = 0; i < arrayreverse.Length; i++)
+                    {
+                        reversedblocks += arrayreverse[i];
+                    }
+                    if (stop == 1)
+                    {
+                        break;
+                    }
+                    addindex += 6;
+                }
+                richTextBox1.Text = reversedblocks;
+                richTextBox2.Text = splitinfRHash.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Invalid Key");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox2.Clear();
+                foreach (char i in richTextBox1.Text)
+                {
+                    textBox2.AppendText((int)i + " ");
+                }
+            }
+            else
+            {
+                textBox2.Clear();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label2.Text = "bits: " + textBox1.Text.Length * 8;
+        }
+
+        private void HashingAlgorithm()
+        {
+            sKHash = "";
+            splitKHash = "";
+            string key = textBox1.Text;
+            int A;
+            int B;
+            int C;
+            int D;
+            int E;
+            int F;
+            int G;
+            int H;
+            long x;
+            long n;
+            long m = 0xFD8E;
+            long c = 0x63;
+            long r = 0;
+            long b = 0;
+            long l = key.Length;
+            long s = 0x0;
+            foreach (char i in ru) r += i;
+            foreach (char i in key) s += i;
+            foreach (char i in ru25) b += i;
+            n = 0;
+            for (int j = 0; j < l; j++)
+            {
+                x = key[j];
+                n += (long)Math.Pow(x, 5) + (long)Math.Pow(x, 4) + (long)Math.Pow(x, 3) + x + 1;
+            }
+            A = (int)(n % m + r);
+            n = 0;
+            for (int j = 0; j < l; j++)
+            {
+                x = key[j];
+                n += x * (long)(Math.Pow(x, 4) + x + 1);
+            }
+            B = (int)(n % m + r);
+            n = 0;
+            for (int j = 0; j < l; j++)
+            {
+                x = key[j];
+                n += x * (long)(Math.Pow(x, 3) - x - 1);
+            }
+            C = (int)(n % m + r);
+            n = 0;
+            for (int j = 0; j < l; j++)
+            {
+                x = key[j];
+                n += x * r * (long)Math.Pow(x, 2) + s;
+            }
+            D = (int)(n % m + r);
+            n = 0;
+            for (int j = 0; j < l; j++)
+            {
+                x = key[j];
+                n += x * (((long)Math.Pow((((x ^ (long)(Math.Pow(x, 0x2) + 0x4 % 0x8) ^ (long)(Math.Pow(x, 0x2) + 0x5 % 0x8) ^ (long)(Math.Pow(x, 0x2) + 0x6 % 0x8) ^ (long)(Math.Pow(x, 2) + 0x6 % 0x8))) + 1 ^ c ^ s + r), 3)) % b * (x + s));
+            }
+            E = (int)(n % m + r);
+            n = 0;
+            for (int j = 0; j < l; j++)
+            {
+                x = key[j];
+                n += x * (((x * r) + (x * s + 1)) ^ ru25[0] ^ ru25[1] ^ ru25[2] ^ ru25[3] ^ ru25[4] ^ ru25[5] ^ ru25[6] ^ ru25[7] ^ ru25[8] ^ ru25[9]) ^ ru25[10] ^ ru25[11] ^ ru25[12] ^ ru25[13];
+                n += n % r;
+                n ^= x;
+            }
+            F = (int)(n % m + r);
+            n = 0;
+            for (int j = 0; j < l; j++)
+            {
+                x = key[j];
+                n -= x;
+                n += (long)Math.Pow(x, 2) * ru[0];
+                n += x * ru[1];
+                n += x * ru[2];
+                n += x * ru[3];
+                n += x * ru[4];
+                n += x * ru[5];
+                n ^= (long)Math.Pow(x, 2) * r;
+            }
+            G = (int)(n % m + r);
+            n = 0;
+            for (int j = 0; j < l; j++)
+            {
+                x = key[j];
+                n += x * r * s;
+                n ^= x * ru[0] + (x ^ ru25[10]);
+                n ^= x * ru[1] + (x ^ ru25[11]);
+                n ^= x * ru[2] + (x ^ ru25[12]);
+                n ^= x * ru[3] + (x ^ ru25[13]);
+                n ^= x * ru[4] + (x ^ r);
+                n ^= x * ru[5] + (x ^ b);
+                n += (x + (s * l * r * b) % (x * r)) * (x + s + r);
+                n %= x * s * r;
+                n -= x;
+            }
+            H = (int)(n % m + r);
+            string prehash = (A.ToString("X4") + B.ToString("X4") + C.ToString("X4") + D.ToString("X4") + E.ToString("X4") + F.ToString("X4") + G.ToString("X4") + H.ToString("X4"));
+            char[] prehash_ = prehash.ToCharArray();
+            Array.Reverse(prehash_);
+            label3.Text = "";
+            foreach(char i in prehash_)
+            {
+                sKHash += i.ToString();
+            }
+            label3.Text = "Hash: " + sKHash + " len: " + (sKHash.Length * 8);
+            int index = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                splitKHash += sKHash[index];
+                index++;
+                splitKHash += sKHash[index];
+                if (splitKHash.Length < 47)
+                {
+                    splitKHash += "|";
+                }
+                index++;
+            }
+            splitKHash2 = splitKHash.Split('|');
+            KHash = splitKHash2.Select(b1 => Convert.ToByte(b1, 16)).ToArray();
+            //A = (((for(int j = 0; j < l; j++) n + x^8 + x^4 + x^3 x + 1) mod n) + r
+            //B = (((for(int j = 0; j < l; j++) n + x^4 + x + 1) mod n) + r
+            //C = (((for(int j = 0; j < l; j++) n + x^3 - x - 1) mod n) + r
+            //D = ((for(int j = 0; j < l; j++) n + rx + s) mod n) + r
+            //E = for(int j = 0; j < l; j++) n + x XOR (x^2 + 4 mod 8) XOR (x^2 + 5 mod 8) XOR (x^2 + 6 mod 8) XOR (x^2 + 7 mod 8)
+            //F = for(int j = 0; j < l; j++) n + x * (((x * r) + (x * s + 1)) XOR ru25[0] XOR ru25[1] XOR ru25[2] XOR ru25[3] XOR ru25[4] XOR ru25[5] XOR ru25[6] XOR ru25[7] XOR ru25[8] XOR ru25[9]) XOR ru25[10] XOR ru25[11] XOR ru25[12] XOR ru25[13] + (n mod r) XOR x
+            //G = for(int j = 0; j < l; j++) (n - x) + (x * ru[0]) + (x * ru[1]) + (x * ru[2]) + (x * ru[3]) + (x * ru[4]) + (x * ru[5]) XOR (x^2 * r)
+            //H = for(int j = 0; j < l; j++) (n + x * r * s) XOR (x * ru[0] + (x XOR ru25[10])) XOR (x * ru[1] + (x XOR ru25[11])) XOR (x * ru[2] + (x XOR ru25[12])) XOR (x * ru[3] + (x XOR ru25[13])) XOR (x * ru[4] + (x XOR r)) XOR (x * ru[5] + (x XOR r)) + ((x + (s * l * r * b) mod (x * r)) * (x + s + r)) mod (x * s * r) - x
+            //hash = [a, b, c, d, e, f, g, h]
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            HashingAlgorithm();
+        }
+    }
+}
